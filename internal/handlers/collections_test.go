665a73963c0efff9c118c80c0d2832631ad5a5a4
Lou Foster <lf6f@virginia.edu>
2018-04-19 16:24:56 -0400

    collection tests passing

--- internal/handlers/collections_test.go
+++ internal/handlers/collections_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -42,23 +43,59 @@ func TestCollectionsIndex(t *testing.T) {
 }
 
 func TestBadCollectionShow(t *testing.T) {
-	// mockDB, mock, err := sqlmock.New()
-	// if err != nil {
-	// 	t.Fatalf("Stub DB connection failed: %s", err)
-	// }
-	// defer mockDB.Close()
-	// sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
-	// app := ApolloHandler{Version: "MOCK", DB: &models.DB{sqlxDB}}
-	// mock.ExpectQuery("SELECT").WillReturnError(errors.New("Collection not found"))
-	//
-	// req, _ := http.NewRequest("GET", "/api/collection", nil)
-	// rr := httptest.NewRecorder()
-	//
-	// params := httprouter.Params{httprouter.Param{"pid", "bad123"}}
-	// app.CollectionsShow(rr, req, params)
-	//
-	// // Check the status code is what we expect.
-	// if status := rr.Code; status != http.StatusNotFound {
-	// 	t.Errorf("Wrong status code: got %v want %v", status, http.StatusOK)
-	// }
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Stub DB connection failed: %s", err)
+	}
+	defer mockDB.Close()
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+	app := ApolloHandler{Version: "MOCK", DB: &models.DB{sqlxDB}}
+	mock.ExpectQuery("SELECT").WillReturnError(errors.New("Collection not found"))
+
+	req, _ := http.NewRequest("GET", "/api/collection", nil)
+	rr := httptest.NewRecorder()
+
+	params := httprouter.Params{httprouter.Param{"pid", "bad123"}}
+	app.CollectionsShow(rr, req, params)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("Wrong status code: got %v want %v", status, http.StatusNotFound)
+	}
+}
+
+func TestCollectionShow(t *testing.T) {
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Stub DB connection failed: %s", err)
+	}
+	defer mockDB.Close()
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+	app := ApolloHandler{Version: "MOCK", DB: &models.DB{sqlxDB}}
+
+	tgt := "uva-an1"
+	rowsPid := sqlmock.NewRows([]string{"id"}).AddRow(1)
+	rows := sqlmock.NewRows([]string{"n.id", "n.parent_id", "n.pid", "n.value", "n.created_at", "n.updated_at", "nn.pid", "nn.value"}).
+		AddRow(1, nil, tgt, "", nil, nil, "uva-ann1", "collection")
+	mock.ExpectQuery("select id from nodes").WillReturnRows(rowsPid)
+	mock.ExpectQuery("SELECT n.id").WithArgs(1, 1).WillReturnRows(rows)
+
+	req, _ := http.NewRequest("GET", "/api/collection", nil)
+	rr := httptest.NewRecorder()
+
+	params := httprouter.Params{httprouter.Param{"pid", tgt}}
+	app.CollectionsShow(rr, req, params)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	if strings.Contains(strings.TrimSpace(rr.Body.String()), "uva-an1") == false {
+		t.Errorf("Response %s does not contain searched PID: %s", rr.Body.String(), tgt)
+	}
 }
--- internal/models/node.go
+++ internal/models/node.go
@@ -9,37 +9,41 @@ import (
 	"net/url"
 	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 // Node is an element in a tree of metadata
 type Node struct {
-	ID        int64     `json:"-"`
-	PID       string    `json:"pid"`
-	Parent    *Node     `json:"-"`
-	Name      *NodeName `json:"name"`
-	Value     string    `json:"value,omitempty"`
-	Children  []*Node   `json:"children,omitempty"`
-	User      *User     `json:"-"`
-	Deleted   bool      `json:"-"`
-	Current   bool      `json:"-"`
-	CreatedAt time.Time `db:"created_at" json:"createdAt"`
-	UpdatedAt time.Time `db:"updated_at" json:"-"`
+	ID        int64      `json:"-"`
+	PID       string     `json:"pid"`
+	Parent    *Node      `json:"-"`
+	Name      *NodeName  `json:"name"`
+	Value     string     `json:"value,omitempty"`
+	Children  []*Node    `json:"children,omitempty"`
+	User      *User      `json:"-"`
+	Deleted   bool       `json:"-"`
+	Current   bool       `json:"-"`
+	CreatedAt time.Time  `db:"created_at" json:"createdAt"`
+	UpdatedAt *time.Time `db:"updated_at" json:"updatedAt,omitempty"`
 }
 
 // MarshalJSON will encode the Node structure as JSON
 func (n *Node) MarshalJSON() ([]byte, error) {
 	return json.Marshal(&struct {
-		PID       string    `json:"pid"`
-		Name      *NodeName `json:"name"`
-		Value     string    `json:"value,omitempty"`
-		Children  []*Node   `json:"children,omitempty"`
-		CreatedAt time.Time `db:"created_at" json:"createdAt"`
+		PID       string     `json:"pid"`
+		Name      *NodeName  `json:"name"`
+		Value     string     `json:"value,omitempty"`
+		CreatedAt time.Time  `db:"created_at" json:"createdAt"`
+		UpdatedAt *time.Time `db:"updated_at" json:"updatedAt,omitempty"`
+		Children  []*Node    `json:"children,omitempty"`
 	}{
 		PID:       n.PID,
 		Name:      n.Name,
 		Value:     encodeValue(n.Value),
-		Children:  n.Children,
 		CreatedAt: n.CreatedAt,
+		UpdatedAt: n.UpdatedAt,
+		Children:  n.Children,
 	})
 }
 
@@ -61,21 +65,22 @@ func (db *DB) GetCollectionPIDs() []string {
 
 // GetCollection returns an entire collection identified by PID
 func (db *DB) GetCollection(pid string) (*Node, error) {
+	log.Printf("Get collecton with PID %s", pid)
+
 	// first, get the root node
+	var rootID int64
+	db.QueryRow("select id from nodes where pid=?", pid).Scan(&rootID)
 	var nodes []*Node
-	root := db.GetNode(pid)
-	nodes = append(nodes, root)
+	var root *Node
+	log.Printf("Collecton for PID %s has ID %d", pid, rootID)
 
 	// now get all children with the root ID as the start of their ancestry
 	qs := fmt.Sprintf(
-		`SELECT n.id, n.parent_id, n.pid, n.value, n.created_at,
-         nn.id, nn.pid, nn.value,
-         u.id, u.computing_id, u.last_name, u.first_name, u.email
+		`SELECT n.id, n.parent_id, n.pid, n.value, n.created_at, n.updated_at, nn.pid, nn.value
        FROM nodes n
          inner join node_names nn on nn.id = n.node_name_id
-         inner join users u on u.id = n.user_id
-       WHERE deleted=0 and current=1 and (ancestry like '%d/%%' or ancestry=?)`, root.ID)
-	rows, err := db.Queryx(qs, root.ID)
+       WHERE deleted=0 and current=1 and (n.id=? or ancestry like '%d/%%' or ancestry=?) order by n.id asc`, rootID)
+	rows, err := db.Queryx(qs, rootID, rootID)
 	if err != nil {
 		log.Printf("ERROR: unable to retrieve collection: %s", err.Error())
 		return nil, err
@@ -84,14 +89,17 @@ func (db *DB) GetCollection(pid string) (*Node, error) {
 	for rows.Next() {
 		var n Node
 		var nn NodeName
-		var u User
 		var parentID sql.NullInt64
-		rows.Scan(&n.ID, &parentID, &n.PID, &n.Value, &n.CreatedAt,
-			&nn.ID, &nn.PID, &nn.Value,
-			&u.ID, &u.ComputingID, &u.LastName, &u.FirstName, &u.Email)
+		var updateAt mysql.NullTime
+		rows.Scan(&n.ID, &parentID, &n.PID, &n.Value, &n.CreatedAt, &updateAt, &nn.PID, &nn.Value)
+		if updateAt.Valid {
+			n.UpdatedAt = &updateAt.Time
+		}
 		n.Name = &nn
-		n.User = &u
 		nodes = append(nodes, &n)
+		if n.ID == rootID {
+			root = &n
+		}
 		if parentID.Valid {
 			// a parentID exists. That node should be found in the nodes array
 			found := false
@@ -114,6 +122,7 @@ func (db *DB) GetCollection(pid string) (*Node, error) {
 }
 
 // GetNode finds a SINGLE node by PID. No parent nor children is returned
+// Details about user and revision history are included (TODO)
 func (db *DB) GetNode(pid string) *Node {
 	qs := `SELECT n.id, n.pid, n.value, n.deleted, n.current, n.created_at,
             nn.id, nn.pid, nn.value,
