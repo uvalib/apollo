83365797cfadf59fabb2d8f48a22ab3470ef3837
Lou Foster <lf6f@virginia.edu>
2018-04-27 14:03:29 -0400

    added a router and fixed project name/description

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 /*jshint esversion: 6 */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
 Vue.config.productionTip = false;
 
@@ -12,6 +13,7 @@ Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 });
