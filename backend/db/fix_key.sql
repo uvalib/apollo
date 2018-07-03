b520545d00575cf19369623a5fe501f824c86efb
Lou Foster <lf6f@virginia.edu>
2018-07-03 15:13:04 -0400

    bigger value column to handle long wsls descs

--- backend/db/fix_key.sql
+++ backend/db/fix_key.sql
@@ -1,2 +1,3 @@
 alter table controlled_values drop foreign key controlled_values_ibfk_1;
 alter table controlled_values add constraint controlled_values_ibfk_1 FOREIGN KEY (node_type_id) REFERENCES node_types(id) ON DELETE CASCADE;
+ALTER TABLE `nodes` CHANGE COLUMN `value` `value` VARCHAR(2048);
