34d718a1c4a0db0303e5fcf66ad703f4a349b119
Lou Foster <lf6f@virginia.edu>
2019-01-04 14:11:44 -0500

    collection details managed by vuex

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ Vue.use(Vuex)
 // root state object. Holds all of the state for the system
 const state = {
   collections: [],
+  collectionDetails: {},
   error: null,
   loading: false
 }
@@ -18,22 +19,72 @@ const getters = {
   collections: state => {
     return state.collections
   },
-
   error: state => {
     return state.error
   },
-
   hasError: state => {
     return state.error != null && state.error != ""
   },
-
   isLoading: state => {
     return state.loading
   },
-  
-  collectionsCount: state => {
-    return state.collections.length
-  }
+  collectionDetails: state => {
+    return state.collectionDetails
+  },
+  collectionFound: state => {
+    return Object.keys(state.collectionDetails).length > 0
+  },
+  isPublished: state => {
+    return state.collectionDetails.publishedAt
+  },
+  publishedAt: state => {
+    return state.collectionDetails.publishedAt
+  },
+  virgoLink: state => {
+    let extPid = ""
+    for (var idx in state.collectionDetails.attributes) {
+      let attr = state.collectionDetails.attributes[idx]
+      if (attr.type.name === "externalPID"){
+        extPid = attr.values[0].value
+        break
+      }
+    }
+    return process.env.VUE_APP_VIRGO_URL+"/catalog/"+extPid
+  },
+  jsonLink: state => {
+    return "/api/collections/"+state.collectionDetails.pid
+  },
+  fromSirsi: state=> {
+    // A collection is from Sirsi if it has a barcode
+    for (var idx in state.collectionDetails.attributes) {
+      let attr = state.collectionDetails.attributes[idx]
+      if (attr.type.name === "barcode") return true
+    }
+    return false
+  },
+  sirsiLink: state => {
+    // This should only return a URL for nodes that
+    // are top level. A top level node will have a barcode and/or key
+    let barcode=""
+    let catalogKey = ""
+    for (var idx in state.collectionDetails.attributes) {
+      let attr = state.collectionDetails.attributes[idx]
+      if (attr.type.name === "barcode"){
+        barcode = attr.values[0].value
+      }
+      if (attr.type.name === "catalogKey") {
+        catalogKey = attr.values[0].value
+      }
+    }
+
+    if (barcode.length > 0) {
+      return process.env.VUE_APP_SOLR_URL+"/core/select/?q=barcode_facet:"+barcode
+    }
+    if (catalogKey.length > 0) {
+      return process.env.VUE_APP_SOLR_URL+"/core/select/?q=id:"+catalogKey
+    }
+    return ""
+  },
 }
 
 // Synchronous updates to the state. Can be called directly in components like this:
@@ -48,6 +99,11 @@ const mutations = {
   setCollections(state, colls) {
     if (colls) {
       state.collections = colls
+    }
+  },
+  setCollectionDetails(state, detail) {
+    if (detail) {
+      state.collectionDetails = detail
     }
   }
 }
@@ -73,18 +129,95 @@ const actions = {
       ctx.commit('setLoading', false) 
     })
   },
+
+  getCollectionDetails(ctx, collectionID) {
+    ctx.commit('setLoading', true) 
+    axios.get("/api/collections/"+collectionID).then((response)  =>  {
+      let model = traverseCollectionDetail(response.data, {})
+      ctx.commit('setCollectionDetails', model)
+    }).catch((error) => {
+      ctx.commit('setCollectionDetails', {}) 
+      if (error.response ) {
+        ctx.commit('setError', error.response.data)
+      } else {
+        ctx.commit('setError', error)
+      }
+    }).finally(() => {
+      ctx.commit('setLoading', false) 
+    })  
+  }
 }
 
-// // Plugin to listen for error messages being set. After a delay, clear them
-// const errorPlugin = store => {
-//   store.subscribe((mutation) => {
-//     if (mutation.type === "setError") {
-//       if ( mutation.payload != null ) {
-//         setTimeout( ()=>{ store.commit('setError', null)}, 6000)
-//       }
-//     }
-//   })
-// }
+// recursive walk of json collection heirarchy into a format more suited 
+// for presntation. Instead of a list of heirarchical nodes, convert to 
+// items and attributes. Items contain other items and attributes. Attributes 
+// are just name/value pairs. Both items and Attributes have some common metadata: 
+// PID, Type and Sequence. 
+function traverseCollectionDetail(json, currNode) {
+  // init data that is common to all node types (and is single instance):
+  // pid, type, sequence and (if present) published date
+  commonInit(json, currNode)
+
+  // Detect and handle container nodes differently; recursively walk their children.
+  // Container nodes contain attributes (simple name/value pairs) and children.
+  // Examples of containers are collection, year and issue.
+  if (json.type.container === true) {
+    // Walk children and build attributes and children arrays
+    for (var idx in json.children) {
+      var child = json.children[idx]
+      if (child.type.container === false) {
+        // This is an attribute; just grab its value (and valueURI)
+        // Important: attributes can be multi-valued. Stuff all values
+        // in an array. Init attribues as a blank array of it doesn't exist
+        if (!currNode.attributes) currNode.attributes = []
+
+        if  (hasAttribute(currNode.attributes, child.type) === false ) {
+          // This is the first instance of this node type. Init a blank
+          // attribute with no values and add it to the list of attributes for this node
+          var attrNode = {}
+          commonInit(child, attrNode)
+          attrNode.values = []
+          currNode.attributes.push( attrNode )
+        }
+
+        // Now grab the value and add it to the array of values for the existing attrinute
+        var val = {value: child.value}
+        if (child.valueURI) {
+          val.valueURI = child.valueURI
+        }
+        attrNode.values.push(val)
+      } else {
+        // This is another container. Traverse it and append results to children list
+        // If this is the first child encountered, create the blank array to hold the children.
+        if (!currNode.children) currNode.children = []
+        var sub = traverseCollectionDetail(child, {})
+        currNode.children.push( sub )
+      }
+    }
+  }
+  return currNode
+}
+
+ // initialize data elements common to both attribue and item nodes
+ function commonInit(json, currNode) {
+  currNode.pid = json.pid
+  currNode.type = json.type
+  currNode.sequence = json.sequence
+  if (json.publishedAt) {
+    currNode.publishedAt = json.publishedAt
+  }
+}
+
+// See of the list of attributes already includes the attribute spacified
+function hasAttribute(attributes, attrType) {
+  for (var idx in attributes) {
+    let attr = attributes[idx]
+    if (attr.type.name == attrType.name) {
+      return true
+    }
+  }
+  return false
+}
 
 // A Vuex instance is created by combining state, getters, actions and mutations
 export default new Vuex.Store({
--- frontend/src/views/CollectionDetails.vue
+++ frontend/src/views/CollectionDetails.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="detail-wrapper">
-    <ApolloError v-if="errorMsg" :message="errorMsg"/>
+    <ApolloError v-if="hasError" :message="error"/>
     <template v-else>
       <PageHeader
         main="Collection Details"
         :sub="title"
       />
-      <LoadingSpinner v-if="loading" message="Loading collection details"/>
-      <div v-else-if="Object.keys(collection).length === 0" class="content">
+      <LoadingSpinner v-if="isLoading" message="Loading collection details"/>
+      <div v-else-if="collectionFound === false" class="content">
         <h4>No data found!</h4>
       </div>
       <template v-else>
@@ -20,15 +20,15 @@
             <div class="toolbar">
               <span @click="publishClicked" class="publish">Publish Collection</span>
               <a class="raw" :href="jsonLink" target="_blank">JSON</a>
-              <a v-if="hasBarcode" class="sirsi" :href="sirsiLink" target="_blank">Sirsi</a>
-              <div v-if="published" class="publication">
+              <a v-if="fromSirsi" class="sirsi" :href="sirsiLink" target="_blank">Sirsi</a>
+              <div v-if="isPublished" class="publication">
                 <span class="label">Last Published:</span>
-                <span class="date">{{ formattedPublishedDate }}</span>
+                <span class="date">{{ formatDateTime(publishedAt) }}</span>
                 <a class="virgo" :href="virgoLink" target="_blank">Virgo</a>
               </div>
             </div>
             <ul class="collection">
-              <CollectionDetailsItem :model="collection" :depth="0"/>
+              <CollectionDetailsItem :model="collectionDetails" :depth="0"/>
             </ul>
           </div>
           <div class="pure-u-15-24">
@@ -54,6 +54,7 @@
   import EventBus from '@/components/EventBus'
   import PinnedScroll from '@/components/PinnedScroll'
   import ApolloError from '@/views/ApolloError'
+  import { mapGetters } from 'vuex'
 
   export default {
     name: 'CollectionDetails',
@@ -65,6 +66,22 @@
       PageHeader
     },
 
+    computed: {
+      ...mapGetters([
+        'isLoading',
+        'hasError',
+        'error',
+        'collectionDetails',
+        'collectionFound',
+        'isPublished',
+        'publishedAt',
+        'virgoLink',
+        'jsonLink',
+        'fromSirsi',
+        'sirsiLink',
+      ]), 
+    },
+
     props: {
       id: String,
       title: String,
@@ -73,10 +90,7 @@
 
     data: function () {
       return {
-        collection: {},
-        loading: true,
         viewerClicked: false,
-        errorMsg: null,
         viewerError: null,
         ancestry: [],
         pinHeader: new PinnedScroll("div.fixed-header", 216),
@@ -84,90 +98,23 @@
       }
     },
 
-    computed: {
-      iiifAvailable: function() {
-        // as of 7/2018 all items in Apollo that have barcodes are images
-        // and have IIIF available. WSLS does not have barcoces, and is not
-        // image-based. Easy check for now;
-        let iiif = this.hasBarcode
-        return iiif
-      },
-      hasBarcode: function() {
-        for (var idx in this.collection.attributes) {
-          let attr = this.collection.attributes[idx]
-          if (attr.type.name === "barcode") return true
-        }
-        return false
-      },
-      published: function() {
-        return this.collection.publishedAt
-      },
-      formattedPublishedDate: function() {
-        let m = moment(this.collection.publishedAt, "YYYY-MM-DDTHH:mm:ssZ")
-        return m.utcOffset("+0000").format("YYYY-MM-DD hh:mma")
-      },
-      jsonLink: function() {
-        return "/api/collections/"+this.collection.pid
-      },
-      sirsiLink: function() {
-        // This should only return a URL for nodes that
-        // are top level. A top level node will have a barcode and/or key
-        let barcode=""
-        let catalogKey = ""
-        for (var idx in this.collection.attributes) {
-          let attr = this.collection.attributes[idx]
-          if (attr.type.name === "barcode"){
-            barcode = attr.values[0].value
-          }
-          if (attr.type.name === "catalogKey") {
-            catalogKey = attr.values[0].value
-          }
-        }
-
-        if (barcode.length > 0) {
-          return process.env.VUE_APP_SOLR_URL+"/core/select/?q=barcode_facet:"+barcode
-        }
-        if (catalogKey.length > 0) {
-          return process.env.VUE_APP_SOLR_URL+"/core/select/?q=id:"+catalogKey
-        }
-        return ""
-      },
-      virgoLink: function() {
-        let extPid = ""
-        for (var idx in this.collection.attributes) {
-          let attr = this.collection.attributes[idx]
-          if (attr.type.name === "externalPID"){
-            extPid = attr.values[0].value
-            break
-          }
-        }
-        return process.env.VUE_APP_VIRGO_URL+"/catalog/"+extPid
-      }
-    },
-
-    created: function () {
-      axios.get("/api/collections/"+this.id).then((response)  =>  {
-        // parse json tree response into the collection model
-        this.traverseDetails(response.data, this.collection)
+    watch: {
+      'collectionDetails': function() {
         if (this.targetPID) {
           // A target was specified; get a list of ancestor nodes
           // that can be used to expand the display tree to that item
-          this.getAncestry(this.collection)
+          this.getAncestry(this.collectionDetails)
           this.ancestry.reverse()
 
           // NOTE: for the first case, need to wait for one more node; the root itself
           // mount events come in for each child followed by one for root node
           this.ancestry[0].childNodeCount+=1
         }
-      }).catch((error) => {
-        if (error.response ) {
-          this.errorMsg =  error.response.data
-        } else {
-          this.errorMsg =  error
-        }
-      }).finally(() => {
-        this.loading = false
-      })
+      }
+    },
+
+    created: function () {
+      this.$store.dispatch('getCollectionDetails', this.id)
     },
 
     mounted: function (){
@@ -185,6 +132,11 @@
     },
 
     methods: {
+      formatDateTime: function( ts ) {
+        let m = moment(ts, "YYYY-MM-DDTHH:mm:ssZ")
+        return m.utcOffset("+0000").format("YYYY-MM-DD hh:mma")
+      },
+      
       // Walk the collection data and find the targetPID specified by the query params
       // Populate an array of ancestry data including node counts for the relevant tree branches
       getAncestry: function(currNode) {
@@ -243,78 +195,11 @@
         let resp = confirm("Publish this collection?")
         if (!resp) return
 
-        axios.post("/api/publish/solr/"+this.collection.pid).then(()  =>  {
+        axios.post("/api/publish/solr/"+this.collectionDetails.pid).then(()  =>  {
           alert("The publication process has been started. The collection will appear in Virgo within 24 hours.")
         }).catch((error) => {
           alert("Unable to publish collection: "+error.response)
         })
-      },
-
-      // initialize data elements common to both attribue and container nodes
-      commonInit: function(json, currNode) {
-        currNode.pid = json.pid
-        currNode.type = json.type
-        currNode.sequence = json.sequence
-        if (json.publishedAt) {
-          currNode.publishedAt = json.publishedAt
-        }
-      },
-
-      // See of the list of attributes already includes an attribute of
-      // the target type specified in tgtType
-      hasAttribute: function(attributes, tgtType) {
-        for (var idx in attributes) {
-          let attr = attributes[idx]
-          if (attr.type.name == tgtType.name) {
-            return true
-          }
-        }
-        return false
-      },
-
-      traverseDetails: function(json, currNode) {
-        // init data that is common to all node types (and is single instance):
-        // pid, type, sequence and (if present) published date
-        this.commonInit(json,currNode)
-
-        // Detect and handle container nodes differently; recursively walk their children.
-        // Container nodes contain attributes (simple name/value pairs) and children.
-        // Examples of containers are collection, year and issue.
-        if (json.type.container === true) {
-          // Walk children and build attributes and children arrays
-          for (var idx in json.children) {
-            var child = json.children[idx]
-            if (child.type.container === false) {
-              // This is an attribute; just grab its value (and valueURI)
-              // Important: attributes can be multi-valued. Stuff all values
-              // in an array. Init attribues as a blank array of it doesn't exist
-              if (!currNode.attributes) currNode.attributes = []
-
-              if  (this.hasAttribute(currNode.attributes, child.type) === false ) {
-                // This is the first instance of this node type. Init a blank
-                // attribute with no values and add it to the list of attributes for this node
-                var attrNode = {}
-                this.commonInit(child, attrNode)
-                attrNode.values = []
-                currNode.attributes.push( attrNode )
-              }
-
-              // Now grab the value and add it to the array of values for the existing attrinute
-              var val = {value: child.value}
-              if (child.valueURI) {
-                val.valueURI = child.valueURI
-              }
-              attrNode.values.push(val)
-            } else {
-              // This is another container. Traverse it and append results to children list
-              // If this is the first child encountered, create the blank array to hold the children.
-              if (!currNode.children) currNode.children = []
-              var sub = this.traverseDetails(child, {})
-              currNode.children.push( sub )
-            }
-          }
-        }
-        return currNode
       },
 
       scrollToTarget: function() {
