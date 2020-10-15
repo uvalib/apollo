83f3df5e00e7bdb5a5aad045e03d210ce748352a
Dave Goldstein <dpg3k@virginia.edu>
2020-10-15 12:57:07 -0400

    Update for changed command line options

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -7,7 +7,7 @@ if [ -n "$APOLLO_DEVUSER" ]; then
 fi
 
 # run from here, since application expects web template in web/ and writes pdfs to tmp/
-cd bin; ./apollo -host $APOLLO_HOST -dbhost $APOLLO_DB_HOST -dbname $APOLLO_DB_NAME -dbuser $APOLLO_DB_USER -dbpass $APOLLO_DB_PASSWD -iiif $APOLLO_IIIF_MAN_URL -solr_dir $APOLLO_SOLR_DROPOFF_DIR -qdc_dir $APOLLO_QDC_DELIVERY_DIR -fedora $APOLLO_WSLS_FEDORA_URL $DEVUSER_OPT
+cd bin; ./apollo -apollo $APOLLO_HOST -dbhost $APOLLO_DB_HOST -dbname $APOLLO_DB_NAME -dbuser $APOLLO_DB_USER -dbpass $APOLLO_DB_PASSWD -iiif $APOLLO_IIIF_MAN_URL $DEVUSER_OPT
 
 #
 # end of file
